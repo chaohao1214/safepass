30873e00741abdd4c002c677faae5d30698f617d
chaohao1214 <zch264999454@gmail.com>
2025-05-11 20:01:50 -0400

    new package validator, validat user model and api

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Invalid email format",
     },
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: [true, "Passowrd is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("User", UserSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const validator = require("validator");
 const User = require("../models/User");
 
 const router = express.Router();
@@ -8,6 +9,17 @@ const router = express.Router();
 // register
 router.post("/register", async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 character longs." });
+  }
   try {
     // check existing users
     const existingUser = await User.findOne({ email });
