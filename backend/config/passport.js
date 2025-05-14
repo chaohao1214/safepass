3cbb3fc912a8fe18441b51f77f09926e1e522fe9
chaohao1214 <zch264999454@gmail.com>
2025-05-14 17:51:50 -0400

    add enviornment in .env file

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 passport.use(
   new GoogleStrategy(
